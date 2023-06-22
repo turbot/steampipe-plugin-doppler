a4bce7ec47dafdace824a7d56361dd180b457509
misraved <ved@turbot.com>
2023-06-22 15:10:54 +0530

    FIx typo

--- doppler/table_doppler_project.go
+++ doppler/table_doppler_project.go
@@ -16,7 +16,7 @@ func tableDopplerProject(ctx context.Context) *plugin.Table {
 		Description: "A Doppler project defines app config and secrets for one service or app.",
 		List: &plugin.ListConfig{
 			Hydrate: getProject,
-			// If the .spc file has an Invalid project id(the project id is not available in the workplace). In that case, if we run the query 'select * from doppler_project', it will throw a not found error.
+			// If the .spc file has an Invalid project id (the project id is not available in the workplace). In that case, if we run the query 'select * from doppler_project', it will throw a not found error.
 			// Since we make the API call for this table using the project ID value specified in the .spc file, ignoring the not found error for this table will result in an empty row being returned. However, this behavior is generally not reported to the user, leaving them unaware of why the table is returning no results.
 			// So not found error has not been ingnored here.
 		},
